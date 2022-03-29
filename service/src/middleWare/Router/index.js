22157b14571a2b72d82e2a0acbb12e4058226812
xiehui <2499248109@qq.com>
2022-03-29 18:16:18 +0800

    动态挂载路由

--- service/src/middleWare/Router/index.js
+++ service/src/middleWare/Router/index.js
@@ -1,11 +1,33 @@
 const Router = require('koa-router')
 const fs = require('fs')
-const path = 'path'
+const path = require('path')
 
 
 const router = new Router ({
     prefix: '/allinone'
 })
+
+/**
+ * 扫描目录，挂载路由
+ */
+ const addRoutes = (routesUrl) => {
+    // 同步方法无所谓的，因为是在服务器跑起来之前就完成映射，不会有任何性能影响
+    const routesDir = fs.readdirSync(routesUrl)
+    console.log(routesDir)
+    routesDir.forEach((filename) => {
+      const file = path.join(routesUrl, '/' + filename)
+      if (fs.lstatSync(file).isDirectory()) {
+        const tdir = path.join(routesUrl, filename)
+        addRoutes(tdir)
+        return
+      }
+      const routerModel = require(path.join(routesUrl, filename))
+      router.use(routerModel.routes(), routerModel.allowedMethods())
+    })
+  }
+  
+  const routePath = path.join(__dirname, '../../router')
+  addRoutes(routePath)
 
 router.get('/', (ctx) => {
     ctx.body = 'Hello  Allinone'
