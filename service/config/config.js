4b0d145f6a88a0208ad0de321c2154bc2d3bc9fb
xiehui <2499248109@qq.com>
2022-03-31 13:52:07 +0800

    连接阿里云mongo数据库

--- service/config/config.js
+++ service/config/config.js
@@ -1,7 +1,8 @@
 const  config = {
     port: 3000,
     mongodb_db:"manageSystem",
-    mongodb_url: 'mongodb://127.0.0.1:27017/manageSystem',
+    // mongodb_url: 'mongodb://127.0.0.1:27017/manageSystem',
+    mongodb_url: 'mongodb://admin:123456@47.103.142.69:27017/manageSystem',
     // mongodb_url: "mongodb+srv://admin:xiehui0602@cluster0.mbb5z.mongodb.net/manageSystem?retryWrites=true&w=majority",
     user: 'admin',
     password: '123456'
