26896715304b83d015fa3cbf27b4ad010aba3c9f
xiehuicc <2499248109@qq.com>
2022-04-04 16:15:17 +0800

    axios封装

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
+import config from '../../static/config'
+axios.defaults.baseURL = config.baseUrl
 
-console.log(xxxconfig.baseURL)
-Vue.prototype.$axios = axios
-axios.defaults.baseURL = xxxconfig.baseURL
+export default {
+  setAxiosGetPromise: (url, params) => params ? axios.get(url, { params: params }) : axios.get(url),
+  setAxiosPostPromise: (url, data) => data ? axios.post(url, data) : axios.post(url),
+  setAxiosPutPromise: (url, data) => data ? axios.put(url, data) : axios.put(url),
+  setAxiosDeletePromise: (url, params) => params ? axios.delete(url, { params: params }) : axios.delete(url)
+}
