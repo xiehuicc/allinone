60111d110876fd2d32dcd6f4a4207088e47a32a7
xiehuicc <2499248109@qq.com>
2022-04-04 16:16:42 +0800

    vux版本问题&控制缩进

--- web/src/main.js
+++ web/src/main.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 import router from './routes/index'
 //导入插件
 import './plugin'
-import axios from 'axios'
-Vue.prototype.$axios = axios
-axios.defaults.baseURL = 'http://47.103.142.69:3000/allinone/'
+import axios from './utils/axios'
+import store from './store/index'
 import App from './App.vue'
 Vue.config.productionTip = false
 
 
+Vue.use(axios)
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
